180984887d8eaae35c8aa789d5eef5905065cded
Wiktor Latanowicz <wiktor@latanowicz.com>
2022-08-01 19:10:09 +0200

    Boto client dry run: parameter valdation

--- ecsctrl/boto_client.py
+++ ecsctrl/boto_client.py
@@ -1,6 +1,12 @@
-import boto3
 import json
+import sys
+from functools import partial
+
+import boto3
 import click
+from botocore.loaders import Loader
+from botocore.model import ServiceModel
+from botocore.validate import ParamValidator
 
 
 class BotoClient:
@@ -10,20 +16,70 @@ class BotoClient:
         self.client = None
         if not dry_run:
             self.client = boto3.client(service)
+        else:
+            self.client = DryBotoClient(service)
 
     def call(self, method, *args, **kwargs):
-        if not self.dry_run:
-            return getattr(self.client, method)(*args, **kwargs)
-        else:
-            json_spec = json.dumps(kwargs, indent=2)
-            click.echo(f"🧸 BOTO: Would call `{self.service}:{method}` with {json_spec}")
-            return self._dry_run_mocked_response(method, *args, **kwargs)
+        return getattr(self.client, method)(*args, **kwargs)
 
-    def _dry_run_mocked_response(self, method, *args, **kwargs):
+
+class DryBotoClient:
+    def __init__(self, service) -> None:
+        self.service = service
+        self.service_model = self._load_service_model(service)
+
+    def __getattr__(self, method):
+        return partial(self._call, method)
+
+    def _call(self, method, **params):
+        json_params = json.dumps(params, indent=2)
+        click.echo(f"🧸 BOTO: Would call `{self.service}:{method}` with {json_params}")
+        self._validate_params(method, params)
+        return self._dry_run_mocked_response(method, params)
+
+    def _validate_params(self, method, parameters):
+        operation_name = self._method_name_to_operation_name(method)
+        operation_model = self.service_model.operation_model(operation_name)
+        input_shape = operation_model.input_shape
+        validator = ParamValidator()
+        if input_shape is not None:
+            report = validator.validate(parameters, input_shape)
+            if report.has_errors():
+                click.echo(
+                    f"⛔️ BOTO: Function `{self.service}:{method}` parameter validation failed."
+                )
+                report_lines = report.generate_report().splitlines()
+                for i, rl in enumerate(report_lines):
+                    click.echo(f"🔴 Validation error {i+1}: " + rl)
+                sys.exit(2)
+
+        click.echo(
+            f"✅ BOTO: Function `{self.service}:{method}` parameter validation passed."
+        )
+
+    def _dry_run_mocked_response(self, method, params):
         if self.service == "ecs":
             if method == "register_task_definition":
                 return {"taskDefinition": {"taskDefinitionArn": "N/A"}}
+            if method == "describe_services":
+                return {"services": [{}]}
+            if method == "update_service":
+                return {"service": {"serviceArn": params["service"]}}
+            if method == "create_service":
+                return {"service": {"serviceArn": params["serviceName"]}}
         if self.service == "ssm":
             if method == "put_parameter":
                 return {"Version": 123}
         return {}
+
+    def _load_service_model(self, service_name, api_version=None):
+        loader = Loader()
+        json_model = loader.load_service_model(
+            service_name, "service-2", api_version=api_version
+        )
+        service_model = ServiceModel(json_model, service_name=service_name)
+        return service_model
+
+    def _method_name_to_operation_name(self, method_name: str):
+        parts = method_name.split("_")
+        return "".join([part.capitalize() for part in parts])
